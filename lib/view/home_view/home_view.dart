f032fa7c834a4fb4cc6e035e4b59ba822e9b86a4
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-03-21 06:27:50 +0300

    miniature minefield widget updated

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -125,11 +125,16 @@ class GameTitle extends StatelessWidget {
   }
 }
 
-class MiniatureMinefield extends StatelessWidget {
+class MiniatureMinefield extends StatefulWidget {
   const MiniatureMinefield({
     super.key,
   });
 
+  @override
+  State<MiniatureMinefield> createState() => _MiniatureMinefieldState();
+}
+
+class _MiniatureMinefieldState extends State<MiniatureMinefield> {
   @override
   Widget build(BuildContext context) {
     var rnd = Random();
@@ -138,42 +143,45 @@ class MiniatureMinefield extends StatelessWidget {
       rnd.nextInt(20),
       rnd.nextInt(20),
     ];
-    return SizedBox(
-      height: 172,
-      child: GridView.builder(
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 10),
-        itemCount: 40,
-        itemBuilder: (BuildContext context, index) {
-          Color? mineColor;
-          bool mineCell = minePlaces.contains(index - 19);
-          if (mineCell) {
-            var rnd = Random();
+    return GestureDetector(
+      onTap: () => setState(() {}),
+      child: SizedBox(
+        height: 172,
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 10),
+          itemCount: 40,
+          itemBuilder: (BuildContext context, index) {
+            Color? mineColor;
+            bool mineCell = minePlaces.contains(index - 19);
+            if (mineCell) {
+              var rnd = Random();
 
-            mineColor = GameColors
-                .mineColors[rnd.nextInt(GameColors.mineColors.length)];
-          }
-          return Container(
-            decoration: BoxDecoration(
-              color: mineCell
-                  ? mineColor
-                  : (index / 10).floor() < 2
-                      ? index % 2 == (index / 10).floor() % 2
-                          ? GameColors.grassLight
-                          : GameColors.grassDark
-                      : index % 2 != (index / 10).floor() % 2
-                          ? GameColors.tileDark
-                          : GameColors.tileLight,
-            ),
-            alignment: Alignment.center,
-            padding: const EdgeInsets.all(8),
-            child: mineCell
-                ? CircleAvatar(
-                    backgroundColor: GameColors.darken(mineColor!),
-                  )
-                : const SizedBox(),
-          );
-        },
+              mineColor = GameColors
+                  .mineColors[rnd.nextInt(GameColors.mineColors.length)];
+            }
+            return Container(
+              decoration: BoxDecoration(
+                color: mineCell
+                    ? mineColor
+                    : (index / 10).floor() < 2
+                        ? index % 2 == (index / 10).floor() % 2
+                            ? GameColors.grassLight
+                            : GameColors.grassDark
+                        : index % 2 != (index / 10).floor() % 2
+                            ? GameColors.tileDark
+                            : GameColors.tileLight,
+              ),
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(8),
+              child: mineCell
+                  ? CircleAvatar(
+                      backgroundColor: GameColors.darken(mineColor!),
+                    )
+                  : const SizedBox(),
+            );
+          },
+        ),
       ),
     );
   }
