f8edb820a16c49199495fcdaefc1703e062e4c96
Recep Oğuzhan Şenoğlu <recep.senoglu.34@gmail.com>
2023-12-03 19:49:23 +0300

    stats appbar fixed

--- lib/utils/game_strings.dart
+++ lib/utils/game_strings.dart
@@ -1,6 +1,16 @@
 class GameStrings {
+  GameStrings._();
+
+  // Home
   static const String appName = 'Minesweeper';
   static const String newGame = 'New Game';
   static const String statistics = 'Statistics';
+
+  // Statistics
+  static const String gameStats = 'Game Stats';
+  static const String easy = 'Easy';
+  static const String medium = 'Medium';
+  static const String hard = 'Hard';
+
 
 }
--- lib/view/statistics_view/statistics_view.dart
+++ lib/view/statistics_view/statistics_view.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:minesweeper/utils/game_colors.dart';
+import 'package:minesweeper/utils/game_sizes.dart';
+import 'package:minesweeper/utils/game_strings.dart';
 
 import '../../utils/game_consts.dart';
-import 'app_bar.dart';
 import 'stats_table.dart';
 
 class StatisticsView extends StatelessWidget {
@@ -9,12 +11,49 @@ class StatisticsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const DefaultTabController(
+    return DefaultTabController(
       length: 3,
       child: Scaffold(
         backgroundColor: Colors.white,
-        appBar: StatsAppBar(),
-        body: TabBarView(children: [
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          toolbarHeight: GameSizes.getHeight(0.08),
+          backgroundColor: GameColors.mainSkyBlue,
+          title: const Text(GameStrings.gameStats),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: GameSizes.getWidth(0.055),
+          ),
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            iconSize: GameSizes.getWidth(0.06),
+            splashRadius: GameSizes.getWidth(0.06),
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              color: Colors.white,
+            ),
+          ),
+          bottom: TabBar(
+            tabs: const [
+              Tab(text: GameStrings.easy),
+              Tab(text: GameStrings.medium),
+              Tab(text: GameStrings.hard),
+            ],
+            indicator: const BoxDecoration(),
+            labelStyle: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w800,
+              fontSize: GameSizes.getWidth(0.042),
+              letterSpacing: 2,
+            ),
+            unselectedLabelStyle: const TextStyle(color: Colors.white60),
+          ),
+        ),
+        body: const TabBarView(children: [
           StatsTable(gameMode: GameMode.easy),
           StatsTable(gameMode: GameMode.medium),
           StatsTable(gameMode: GameMode.hard),
